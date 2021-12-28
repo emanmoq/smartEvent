bf22c584c731814d85e7c9938638bd0437b9e0c9
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-12-28 09:12:10 -0800

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,32 @@
 jQuery(function ($) {
-
-
-
+    //hero Slider
+    $('.heroSlider').owlCarousel({
+        autoplay: true,
+        loop: true,
+        margin: 0,
+        rtl:true,
+        dots:false,
+        items:1
+      });
+      $(".gallerySlider").owlCarousel({
+        autoplay: true,
+        loop: true,
+        margin: 0,
+        rtl:true,
+        dots:false,
+        margin:15,
+        responsive:{
+          0:{
+            items:1
+          },
+          600:{
+            items:2
+          },
+          1000:{
+            items:5
+          }
+        }
+      });
 });
+
 
