1d8fdeb21fb662026d9cecca54c95f2a62e6362a
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-12-28 23:04:44 -0800

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,71 @@
 jQuery(function ($) {
-    //hero Slider
-    $('.heroSlider').owlCarousel({
-        autoplay: true,
-        loop: true,
-        margin: 0,
-        rtl:true,
-        dots:false,
-        items:1
-      });
-      $(".gallerySlider").owlCarousel({
-        autoplay: true,
-        loop: true,
-        margin: 0,
-        rtl:true,
-        dots:false,
-        margin:15,
-        responsive:{
-          0:{
-            items:1
-          },
-          600:{
-            items:2
-          },
-          1000:{
-            items:5
-          }
-        }
-      });
+  //hero Slider
+  $('.heroSlider').owlCarousel({
+    autoplay: true,
+    loop: true,
+    margin: 0,
+    rtl: true,
+    dots: false,
+    items: 1
+  });
+  $(".gallerySlider").owlCarousel({
+    autoplay: true,
+    loop: true,
+    rtl: true,
+    dots: false,
+    margin: 30,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 5
+      }
+    }
+  });
+  $(".partnerSlider").owlCarousel({
+    autoplay: true,
+    loop: true,
+    rtl: true,
+    dots: false,
+    margin: 60,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      
+      },
+      600: {
+        items: 5
+      },
+      1000: {
+        items: 7
+      }
+    }
+  });
+  $(".clientsSlider").owlCarousel({
+    autoplay: true,
+    loop: true,
+    rtl: true,
+    dots: false,
+    margin:30,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav:false,
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
 });
 
 
