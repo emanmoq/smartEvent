17cda9a7486ea9b57195cf98f99f64dd2a0211b7
emanmoq <42995029+emanmoq@users.noreply.github.com>
2021-12-30 08:51:45 -0800

    loaderEn

--- assets/js/mainEn.js
+++ assets/js/mainEn.js
@@ -108,11 +108,11 @@ jQuery(function ($) {
     setTimeout(removeLoader, 2000); //wait for page load PLUS two seconds.
   });
   function removeLoader(){
-      $( "#loadingDiv" ).fadeOut(500, function() {
-        // fadeOut complete. Remove the loading div
-        $( "#loadingDiv" ).remove(); //makes page more lightweight 
-    });  
-  }
+    $( ".loaderPage" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( ".loaderPage" ).remove(); //makes page more lightweight 
+  });  
+}
 });
 
 
